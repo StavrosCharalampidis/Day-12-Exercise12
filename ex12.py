59e91a135b5db7b5bd119e29e0e8a8d134f175ef
StavrosCharalampidis <stavroscharalampidis1@gmail.com>
2024-02-14 03:26:22 +0200

    (Try input more) Complete

---
+++ ex12.py
@@ -0,0 +1,14 @@
+# The Name Store a input fild and inside the input has a comment to see the user 
+name = input("Type your name? ")
+
+# The age Store a input fild and inside the input has a comment to see the user 
+age = int(input("Type your age? "))
+
+# The weigh Store a input fild and inside the input has a comment to see the user 
+weigh = input("Type your weigh? ")
+
+# The favNum Store a input fild and inside the input has a comment to see the user 
+favNum = int(input("Type your favorite namber?"))
+
+# Print all the input variables
+print(f'Hello {name} yor age is {age} and your weigh is {weigh} and yor favorite num minus your age is {favNum - age}')
